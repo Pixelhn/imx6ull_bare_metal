07f9b4aff1da40b3499a686f35a779b233290975
lhn_wsl <pixelhn@qq.com>
2023-02-20 20:15:59 +0800

    修改工程结构

--- env.sh
+++ env.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 PATH=$PATH:/home/lhn_de/imx6ull/tool/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/bin
+
+export ARMGCC_DIR=/home/lhn_de/imx6ull/tool/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi
